d38058e9887bec8fba81e28626416232cb7079b5
Zoran Plesivčak <zplesiv@gmail.com>
2015-01-04 14:31:50 +0100

    Reorganized files

---
+++ hac/__init__.py
@@ -0,0 +1,13 @@
+"""
+hac - Helper for Algorithm Competitions
+
+"""
+__author__ = 'Zoran Plesivčak'
+__version__ = '0.1.0-dev'
+__licence__ = 'GPL-2'
+
+
+class ExitStatus:
+    """Exit status code constants."""
+    OK = 0
+    ERROR = 1
